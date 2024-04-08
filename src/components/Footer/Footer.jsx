7ce9392b9e99744624e3993966a75e405fa51d29
sirisayshello <veckovis.salongen0p@icloud.com>
2024-04-08 11:44:51 +0200

    update footer and hero

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,11 @@
 export const Footer = () => {
   return (
-    <div className="w-full mt-20">
-      <div className="border-t-2 border-black px-16 py-8">
+    <div className="w-full mt-16">
+      <div className="border-t border-black px-4 py-8">
         <h2>ABOUT</h2>
         <h2>PREVIOUS WORK</h2>
-        <h2>PRESS</h2>
       </div>
-      <div className="border-t-2 border-black px-16 py-8">
+      <div className="border-t border-black px-4 py-8">
         <h2>BASED IN GOTHENBURG</h2>
       </div>
     </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 export const Header = () => {
   return (
-    <div className="flex justify-between p-4">
-      <p>MENY</p>
-      <h1>SIRI SJÖLIN</h1>
-      <p>VARUKORG</p>
+    <div className="fixed w-full">
+      <div className="flex justify-between p-6 bg-white">
+        <p>MENY</p>
+        <h1>SIRI SJÖLIN</h1>
+        <p>VARUKORG</p>
+      </div>
     </div>
   );
 };
