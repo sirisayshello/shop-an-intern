36eecd0c4bc0d4257c070cfedd531bcd13750433
sirisayshello <veckovis.salongen0p@icloud.com>
2024-04-11 23:05:00 +0200

    product card update

---
+++ src/components/Products/Bag.jsx
@@ -0,0 +1,22 @@
+export const Bag = ({ visibility }) => {
+  return (
+    <svg
+      className={visibility}
+      width="15"
+      height="20"
+      viewBox="0 0 15 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        fillRule="evenodd"
+        clipRule="evenodd"
+        d="M10.916 5.88232C10.9709 5.60075 11 5.30582 11 5C11 2.68915 9.3365 1 7.5 1C5.6635 1 4 2.68915 4 5C4 5.30582 4.02913 5.60075 4.08395 5.88232H3.06984C3.02395 5.59591 3 5.30106 3 5C3 2.23858 5.01472 0 7.5 0C9.98528 0 12 2.23858 12 5C12 5.30106 11.9761 5.59591 11.9302 5.88232H10.916ZM7.51539 9.99997H7.48461C7.48974 9.99999 7.49487 10 7.5 10C7.50513 10 7.51026 9.99999 7.51539 9.99997Z"
+        fill="black"
+      />
+      <rect x="0.5" y="5.5" width="14" height="14" stroke="black" />
+      <line x1="11.5" y1="5" x2="11.5" y2="9" stroke="black" />
+      <line x1="3.5" y1="5" x2="3.5" y2="9" stroke="black" />
+    </svg>
+  );
+};
--- src/components/Products/Product.jsx
+++ src/components/Products/Product.jsx
@@ -1,8 +1,18 @@
+import { Bag } from "./Bag";
+
 export const Product = ({ name, description }) => {
   return (
     <div className="flex flex-col pb-4 w-1/2 lg:w-1/4">
-      <img className="w-full h-full" src="assets/img/product.png" alt="" />
-      <div className="flex flex-col p-4">
+      <div className="relative group w-full h-full flex flex-col overflow-hidden">
+        <img className="w-full h-full" src="assets/img/product.png" alt="" />
+        <div className="hidden [@media(hover:hover)]:flex absolute w-full bottom-[-3rem] group-hover:bottom-0 transition-all bg-white p-4 justify-center items-center">
+          <Bag />
+        </div>
+        <div className="absolute bottom-0 right-0 h-12 px-4 flex justify-end items-center [@media(hover:hover)]:hidden">
+          <Bag />
+        </div>
+      </div>
+      <div className="flex flex-col p-4 h-32 bg-white">
         <h3>{name}</h3>
         <p className="text-xs text-gray-600 lg:text-base">{description}</p>
       </div>
