3d6d1e8ec77c050483f3e8366b4e902e731a885e
sirisayshello <veckovis.salongen0p@icloud.com>
2024-04-23 13:29:43 +0200

    add check component

---
+++ src/components/Products/Check.jsx
@@ -0,0 +1,26 @@
+export const Check = ({ visibility }) => {
+  return (
+    <svg
+      className={visibility}
+      width="22"
+      height="20"
+      viewBox="0 0 26 18"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M10.1243 17.1143L24.876 0.88506"
+        stroke="black"
+        strokeLinecap="square"
+        strokeLinejoin="round"
+      />
+      <line
+        x1="1.20866"
+        y1="9.75308"
+        x2="10.3619"
+        y2="17.6175"
+        stroke="black"
+      />
+    </svg>
+  );
+};
